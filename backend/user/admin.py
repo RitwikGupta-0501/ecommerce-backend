477d20aca95e38c8083e9dee935b97af119aba20
Ritwik Gupta <ritwikg.0501@gmail.com>
2026-03-26 15:40:14 +0530

    FIX: Update user admin page to use unfold

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from . import models
 
+
 # Register your models here.
-admin.site.register(models.User)
+@admin.register(models.User)
+class UserAdmin(ModelAdmin):
+    list_display = ("email", "first_name", "last_name", "company_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+
